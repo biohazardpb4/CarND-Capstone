f55532fa78bb41d1be4c769ef39ed898dd6098ad
root <prombus@gmail.com>
2019-04-15 01:15:00 +0000

    creates script to initialize training

--- traffic_light/dowload_training_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://www.dropbox.com/s/vaniv8eqna89r20/alex-lechner-udacity-traffic-light-dataset.zip?dl=0
-unzip alex-lechner-udacity-traffic-light-dataset.zip?dl=0 ## Don't miss the ``?dl=0`` part when unzipping!
-
---
+++ traffic_light/train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python train.py --logtostderr --train_dir=./train --pipeline_config_path=./ssd_inception_v2_coco.config
+
