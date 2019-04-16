0cda0b7e095768fcf8484e7f2b709095d2faf573
Jay Prombo <prombo@google.com>
2019-04-15 23:03:06 -0400

    updates traffic light detection notebook to draw traffic signal colors around detected lights

--- traffic_light/download_model.sh
+++ traffic_light/download_model.sh
@@ -2,5 +2,3 @@
 
 wget http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_11_06_2017.tar.gz
 tar -zxvf ssd_inception_v2_coco_11_06_2017.tar.gz
-
-# also use 'scp -i ~/car/car-util/car.pem dataset-sdcnd-capstone.zip ubuntu@ec2-52-90-192-175.compute-1.amazonaws.com:~' to get other data
