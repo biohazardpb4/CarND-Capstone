74448ca2ad13465afed79e3614d1104ec852bc0b
Jay Prombo <prombo@google.com>
2019-04-16 00:51:58 -0400

    adds tensorflow inference for traffic light colors!

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,42 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import rospy
+from collections import defaultdict
+import os
+
+GRAPH_FILE = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'frozen_inference_graph_v0.pb')
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        """Loads a frozen inference graph"""
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(GRAPH_FILE, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+
+    def filter_boxes(self, min_score, boxes, scores, classes):
+        """Return: boxes with min_score threshold applied"""
+        n = len(classes)
+        idxs = []
+        for i in range(n):
+            if scores[i] >= min_score:
+                idxs.append(i)
+        
+        filtered_boxes = boxes[idxs, ...]
+        filtered_scores = scores[idxs, ...]
+        filtered_classes = classes[idxs, ...]
+        return filtered_boxes, filtered_scores, filtered_classes
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +48,24 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        light = TrafficLight.UNKNOWN
+        with tf.Session(graph=self.detection_graph) as sess:
+            image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
+            (boxes, scores, classes) = sess.run(
+                [self.detection_boxes, self.detection_scores, self.detection_classes],
+                feed_dict={self.image_tensor: image_np})
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes)
+            boxes, scores, classes = self.filter_boxes(0.5, boxes, scores, classes)
+            for c in classes:
+                class_id = int(c)
+                # 1 = green, 2 = red, 3 = yellow, 4 = unknown
+                labels = {1: 'green', 2: 'red', 3: 'yellow'}
+                if class_id in labels:
+                    rospy.logerr("traffic light label: %s", labels[class_id])
+                else:
+                    rospy.logerr("traffic light class not found: %d", class_id)
+                if class_id == 2:
+                    return TrafficLight.RED
         return TrafficLight.UNKNOWN
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -38,8 +38,8 @@ class TLDetector(object):
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
         
         # Haaack -- wire pose updates to the image callback so that we can save resources
-        sub6 = rospy.Subscriber('/current_pose', PoseStamped, self.image_cb)
-        # sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
+        # sub6 = rospy.Subscriber('/current_pose', PoseStamped, self.image_cb)
+        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
@@ -80,6 +80,7 @@ class TLDetector(object):
         self.has_image = True
         # Haaaack -- subscribe to pose updates so that we save resources.
         # self.camera_image = msg
+        self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
 
         '''
@@ -125,16 +126,16 @@ class TLDetector(object):
 
         """
         # until classification is built out, use the light state provided by the sim
-        return light.state
+        # return light.state
 
-        # if(not self.has_image):
-        #     self.prev_light_loc = None
-        #     return False
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        # #Get classification
-        # return self.light_classifier.get_classification(cv_image)
+        #Get classification
+        return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
